5561b40e05a0f15bd41a5e54abd34d4065d1a17b
22asingh <22asingh@athenian.org>
2020-11-04 04:40:31 +0000

    Created GroceryList and GroceryListClient classes

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -9,11 +9,41 @@ public class GroceryList {
 
     //add
     public void add(String item){
-        for (int i = 0; i < 10; i ++){
+        for (int i = 0; i < this.arr.length - 1; i ++){
             if (this.arr[i] == null){
                 this.arr[i] = item;
                 break;
             }
         }
+    }
+
+    //remove
+    public void remove(String item){
+        for (int i = 0; i < this.arr.length - 1; i ++){
+            if (this.arr[i].equals(item)){
+                this.arr[i] = null;
+                for (int x = i; x < this.arr.length - 1; x ++){
+                    this.arr[x] = this.arr[x+1];
+                }
+                break;
+            }
+        }
+    }
+
+    //toString
+    public String toString(){
+        String str = "Grocery List: ";
+        for (int i = 0; i < this.arr.length - 1; i ++){
+            if (this.arr[i] == null){
+                break;
+            }
+            if (this.arr[i + 1] == null){
+                str += this.arr[i];
+            }
+            else{
+                str += this.arr[i] + ", ";
+            } 
+        }
+        return str;
     }
 }
